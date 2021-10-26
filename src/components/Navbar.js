1abd3d7cb267d12233050a4bd7a5bd79988bbc6c
Franklin-raph <igboekwulusifranklin@gmail.com>
2021-10-26 11:34:16 +0100

    fixed the css file and the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import newcmd from '../assets/newcmd.png';
 import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faUserCircle, faSignInAlt, faDoorOpen, faEye } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faUserCircle, faSignInAlt, faEye } from '@fortawesome/free-solid-svg-icons';
 
 class Navbar extends React.Component {
 
